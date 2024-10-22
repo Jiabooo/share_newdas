527d62efd459b4c7fcfeddb6a1686cca67262aa8
Jiabooo <liujiabo20030810@163.com>
2024-10-22 16:13:30 +0800

    add test

---
+++ das/mask_depth3/test_all_new.py
@@ -0,0 +1,91 @@
+import h5py
+import scipy.io as io
+import PIL.Image as Image
+import numpy as np
+import os
+import glob
+from matplotlib import pyplot as plt
+from scipy.ndimage import gaussian_filter
+import scipy
+import json
+import torchvision.transforms.functional as F
+from matplotlib import cm as CM
+from image import *
+from model import CSRNet
+from model1 import CSRNet1
+import torch
+from matplotlib import cm as c
+from torchvision import datasets, transforms
+from utils import cal_para
+import dataset
+from torch.autograd import Variable
+
+transform = transforms.Compose([
+    transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                std=[0.229, 0.224, 0.225]),
+])
+
+
+def main():
+    with open("A_test.json", 'r') as outfile:
+        test_list = json.load(outfile)
+
+    model = CSRNet()
+    pretrained = torch.load(r"G:\renqun\das\das\mask_depth\mask_depth.tar") # 最后那个best模型的路径
+    model = model.cuda()
+    model.load_state_dict(pretrained['state_dict'])
+
+    mask_model = CSRNet1()
+    pretrained = torch.load(r"G:\renqun\das\das\mask_depth\mask_depth.tar") # second_A路径，用于掩膜
+    mask_model = mask_model.cuda()
+    mask_model.load_state_dict(pretrained['state_dict'])
+
+    test(test_list, model, mask_model)
+
+
+def test(test_list, model, mask_model):
+    print('begin test')
+    test_loader = torch.utils.data.DataLoader(
+        dataset.listDataset(test_list,
+                            transform=transforms.Compose([
+                                transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                                            std=[0.229, 0.224, 0.225]),
+                            ]),
+                            train=False),
+        shuffle=False,
+        batch_size=1)
+
+    model.eval()
+    mae = 0
+    mse = 0
+    all = 0
+    for i, (img,target, count_target, mask_target,depth) in enumerate(test_loader):
+        img = img.cuda()
+        img = Variable(img)
+
+        with torch.no_grad():
+            output1, mask1 = mask_model(img)
+            # output1 = output1/down
+            mask1 = torch.where(mask1 > 0.01, 1, 0)
+            output1 = torch.where(output1 > 0.01, 1, 0)
+            depth = depth.type(torch.FloatTensor).unsqueeze(0).cuda() * output1
+
+            # output,mask = model(img,mask1,depth)
+            output, mask = model(img, depth, mask1)  # normal forword
+            # output = output / down
+
+        # target_sum = (target.sum().type(torch.FloatTensor).cuda() + count_target.sum().type(
+        # torch.FloatTensor).cuda()) / 2
+        target_sum = target.sum().type(torch.FloatTensor).cuda()
+        mae += abs(output.data.sum() - target_sum)
+        mse += (output.data.sum() - target_sum).pow(2)
+
+    N = len(test_loader)
+    mae = mae / N
+    mse = torch.sqrt(mse / N)
+    print(all)
+    print(' * MAE {mae:.3f} \t    * MSE {mse:.3f}'
+          .format(mae=mae, mse=mse))
+
+
+main()
